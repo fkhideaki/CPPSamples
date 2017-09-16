472884dedae4d5f219754a8c99a0b87e32c8b469
qw_fuku <fkhideaki@gmail.com>
2017-09-17 00:42:20 +0900

    create file shortcut sample

---
+++ File/CreateFileShortcut.cpp
@@ -0,0 +1,61 @@
+#include "winnls.h"
+#include "shobjidl.h"
+#include "objbase.h"
+#include "objidl.h"
+#include "shlguid.h"
+
+
+class FileShortcut
+{
+public:
+	static bool Create(
+		const WCHAR* srcPath,
+		const WCHAR* lnkPath,
+		const WCHAR* desc);
+
+	static bool Create(
+		const WCHAR* srcPath,
+		const WCHAR* lnkPath);
+};
+
+
+bool FileShortcut::Create(
+	const WCHAR* srcPath,
+	const WCHAR* lnkPath,
+	const WCHAR* desc)
+{
+	HRESULT hres;
+
+	IShellLink* psl;
+	hres = CoCreateInstance(
+		CLSID_ShellLink,
+		NULL,
+		CLSCTX_INPROC_SERVER,
+		IID_IShellLink,
+		(LPVOID*)&psl);
+
+	if (!SUCCEEDED(hres))
+		return false;
+
+	psl->SetPath(srcPath);
+	psl->SetDescription(desc);
+
+	IPersistFile* ppf;
+	hres = psl->QueryInterface(IID_IPersistFile, (LPVOID*)&ppf);
+
+	if (SUCCEEDED(hres))
+	{
+		hres = ppf->Save(lnkPath, TRUE);
+		ppf->Release();
+	}
+	psl->Release();
+
+	return true;
+}
+
+bool FileShortcut::Create(
+	const WCHAR* srcPath,
+	const WCHAR* lnkPath)
+{
+	return Create(srcPath, lnkPath, 0);
+}
