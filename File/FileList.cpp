e225124508a9f6d80e3219ee9ec0fe21c2b50b2b
qw_fuku <fkhideaki@gmail.com>
2017-09-16 23:22:04 +0900

    c++ get file list sample

---
+++ File/FileList.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+template<typename VisitorFunc>
+void visitFiles(const char* filter, VisitorFunc visitor)
+{
+	struct _finddata_t c_file;
+
+	intptr_t hFile = _findfirst(filter, &c_file);
+	if (hFile == -1L)
+		return;
+
+	for (;;) {
+		visitor(c_file.name);
+		if (_findnext(hFile, &c_file) != 0)
+			break;
+	}
+
+	_findclose(hFile);
+}
+
+void getFiles(const char* filter, std::vector<std::string>& files)
+{
+	files.clear();
+	struct _finddata_t c_file;
+
+	intptr_t hFile = _findfirst(filter, &c_file);
+	if (hFile == -1L)
+		return;
+
+	for (;;) {
+		files.push_back(c_file.name);
+		if (_findnext(hFile, &c_file) != 0)
+			break;
+	}
+
+	_findclose(hFile);
+}
